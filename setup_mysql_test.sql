1f4ad33e0c9af076f8c662e0942a6c5c38f46f61
root <ahmedelamin42@gmail.com>
2024-04-20 04:59:55 -0700

    F

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,22 +1,17 @@
--- Create the database if it doesn't already exist
+-- Use the root user to run this script.
+-- Make sure that the root user has sufficient privileges to create databases and manage users.
+
+-- Create the test database if it does not already exist.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Create the user if it doesn't already exist
+-- Create the user with specified credentials if it does not exist.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant the user all privileges on the hbnb_test_db database
+-- Grant the user all privileges on the test database.
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
--- Revoke all privileges from all databases to clean up any previously granted privileges
-REVOKE ALL PRIVILEGES ON *.* FROM 'hbnb_test'@'localhost';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';  -- This grants the right to connect without any database privileges
-
--- Grant SELECT privilege on the performance_schema database
+-- Ensure the user has only SELECT privilege on the performance_schema database.
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
--- Apply the new privileges
+-- Refresh the privileges to ensure all changes take effect immediately.
 FLUSH PRIVILEGES;
-
--- Commands for verification (to be run after executing the script):
--- echo "SHOW DATABASES;" | mysql -uhbnb_test -p | grep hbnb_test_db
--- echo "SHOW GRANTS FOR 'hbnb_test'@'localhost';" | mysql -uroot -p
