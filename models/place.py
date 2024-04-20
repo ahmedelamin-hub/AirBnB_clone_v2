d9f7860037f8b828d410448fe97c9a87b4822238
Ahmed <ahmedelamin42@gmail.com>
2024-04-20 10:13:59 -0700

    g

--- models/place.py
+++ models/place.py
@@ -19,3 +19,13 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     user = relationship("User", back_populates="places")
     city = relationship("City", back_populates="places")
+    reviews = relationship("Review", back_populates="place", cascade="all, delete")
+
+    if 'HBNB_TYPE_STORAGE' not in globals() or HBNB_TYPE_STORAGE != 'db':
+        @property
+        def reviews(self):
+            """Returns the list of Review instances with place_id equals to the current Place.id"""
+            from models import storage
+            all_reviews = storage.all(Review)
+            place_reviews = [review for review in all_reviews.values() if review.place_id == self.id]
+            return place_reviews
