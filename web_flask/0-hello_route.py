b8cf976862372944fe1fd5ae9839c0319f1b5f5d
root <ahmedelamin42@gmail.com>
2024-05-18 07:23:51 -0700

    g

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,9 @@ A simple Flask web application that displays "Hello HBNB!".
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -13,6 +15,7 @@ def hello_hbnb():
     Route to display 'Hello HBNB!'.
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A Flask web application that displays different messages
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route to display 'Hello HBNB!' at the root URL.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route to display 'HBNB' at the /hbnb URL.
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
